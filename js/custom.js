6196cf9750170478d3543224f2d6596f867ce45b
Rasmusbaywendt <Rasmusbaywendt@gmail.com>
2022-05-17 10:48:53 +0200

    commit 17/05

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,21 @@ function copyToClipboard() {
     alert("Copied to clipboard");
   });
 }
+
+function myFunction1() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function myFunction2() {
+  var x = document.getElementById("myDIV2");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
